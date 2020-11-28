7f28dfab9206d67d07f9bbc797ef97d81117a030
zombimaki <andrewbperez@gmail.com>
2020-11-28 12:11:28 -0600

    done and done

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
-#####################################################
+##########################################################################################################
 # Import Dependencies
-#####################################################
+##########################################################################################################
 
 from flask import Flask, jsonify
 
@@ -9,9 +9,11 @@ from sqlalchemy.ext.automap import automap_base
 from sqlalchemy.orm import Session
 from sqlalchemy import create_engine, func, inspect
 
-#####################################################
+import datetime as dt
+
+##########################################################################################################
 # Connect to Database
-#####################################################
+##########################################################################################################
 
 engine = create_engine("sqlite:///Resources/hawaii.sqlite")
 Base = automap_base()
@@ -21,13 +23,16 @@ session = Session(engine)
 Measurement = Base.classes.measurement
 Station = Base.classes.station
 
-#####################################################
+##########################################################################################################
 # Flask Routes
-#####################################################
+##########################################################################################################
 
 # start flask
 app = Flask(__name__)
 
+##########################################################################################################
+# root route
+##########################################################################################################
 @app.route("/")
 def home():
     return (
@@ -37,18 +42,20 @@ def home():
         f"Returns station and name for all records in the dataset:<br/>"
         f"http://127.0.0.1:5000//api/v1.0/stations<br/>"
         f"<br/>"
-        f"Returns date and tobs for the most active station in the dataset:<br/>"
+        f"Returns date and tobs for the most active station in the dataset over the last 12 months of the dates in the dataset:<br/>"
         f"http://127.0.0.1:5000//api/v1.0/tobs<br/>"
         f"<br/>"
-        f'Enter the start date to retrieve the TMIN, TMAX, and TAVG for all dates after the start date:<br/>'
-        f"http://127.0.0.1:5000//api/v1.0/YYYY-MM-DD<br/>"
+        f'Enter the start date (YYYY-MM-DD) to retrieve the TMIN, TMAX, and TAVG for all dates after the start date. Example:<br/>'
+        f"http://127.0.0.1:5000//api/v1.0/2016-01-01<br/>"
         f"<br/>"
-        f'Enter the start date and the end date:<br/>'
-        f"http://127.0.0.1:5000//api/v1.0/YYYY-MM-DD/YYYY-MM-DD"
+        f'Enter the start date and end dates (YYYY-MM-DD). Example:<br/>'
+        f"http://127.0.0.1:5000//api/v1.0/2016-01-01/2016-01-31"
     )
 
-# # precipitation
-
+##########################################################################################################
+# precipitation route
+## Convert the query results to a dictionary using `date` as the key and `prcp` as the value.
+##########################################################################################################
 @app.route("/api/v1.0/precipitation")
 def precipitation():
     """Convert the query results to a dictionary using `date` as the key and `prcp` as the value."""
@@ -67,11 +74,14 @@ def precipitation():
          prcp_dict_list.append(prcp_dict)
 
     return jsonify(prcp_dict_list)
-    
 
+##########################################################################################################    
+# station route 
+## Return a JSON list of stations from the datase
+##########################################################################################################
 @app.route("/api/v1.0/stations")
 def stations():
-    """Return a JSON list of stations from the dataset."""
+
     session = Session(engine)
 
     station_query =   session.query(Station.station, Station.name).\
@@ -89,15 +99,22 @@ def stations():
 
     return jsonify(station_dict_list)
 
-
+##########################################################################################################
+# tobs route
+## Query the dates and temperature observations of the most active station for the last year of data.
+##########################################################################################################
 @app.route("/api/v1.0/tobs")
-def tobs():
-#     """Query the dates and temperature observations of the most active station for the last year of data."""
+def tobs():    
 
     session = Session(engine)
 
-    active_stations = session.query(Measurement.station,\
-                        func.count(Measurement.id)).\
+    # get the date from 12 months ago
+    max_date_str = session.query(Measurement.date).order_by(Measurement.date.desc()).first()
+    max_date = dt.datetime.strptime(max_date_str[0], '%Y-%m-%d')
+    prior_year_date = dt.date(max_date.year -1, max_date.month, max_date.day)
+
+    active_stations = session.query(Measurement.station,func.count(Measurement.id)).\
+                        filter(Measurement.date >= prior_year_date).\
                         group_by(Measurement.station).\
                         order_by(func.count(Measurement.date).desc()).all()
     
@@ -118,14 +135,14 @@ def tobs():
          tobs_dict["tobs"] = tobs
          tobs_dict_list.append(tobs_dict)
 
-    return jsonify(tobs_dict_list)
+    return jsonify(tobs_dict_list)   
 
-    
-
-
+##########################################################################################################
+# Start Date Route
+## When given the start only, calculate `TMIN`, `TAVG`, and `TMAX` for all dates >= to the start date.
+##########################################################################################################
 @app.route("/api/v1.0/<start>")
 def t_start(start):
-# When given the start only, calculate `TMIN`, `TAVG`, and `TMAX` for all dates >= to the start date.
     
     session = Session(engine)
 
@@ -147,10 +164,12 @@ def t_start(start):
 
     return jsonify(t_start_dict_list)
 
-
+##########################################################################################################
+# Start Date/End Date Route
+## When given start and end dates, calculate `TMIN`, `TAVG`, and `TMAX` for dates between start & end date
+##########################################################################################################
 @app.route("/api/v1.0/<start>/<end>")
 def t_start_end(start,end):
-#  When given the start and the end date, calculate the `TMIN`, `TAVG`, and `TMAX` for dates between the start and end date inclusive."""
 
     session = Session(engine)
 
